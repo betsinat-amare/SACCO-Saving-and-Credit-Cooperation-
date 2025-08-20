0295c1db5cd87f02ba0a053ff96732712fc92ccd
Betsinat Amare <amarebetsinat@gmail.com>
2025-08-20 20:59:02 +0300

    folder structure update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const errorHandler = require("./middleware/errorMiddleware");
+const errorHandler = require("./middleware/errormiddleware");
 
 dotenv.config();
 const app = express();
